c4a990195e31074db92640b62ddcb195318906c8
danielefongo <danielefongo@gmail.com>
2021-05-31 12:20:53 +0200

    add set, set!, get and get!

--- lib/attributes.ex
+++ lib/attributes.ex
@@ -1,18 +1,69 @@
 defmodule Attributes do
-  @moduledoc """
-  Documentation for `Attributes`.
-  """
+  @moduledoc false
+  @attributes_field :__attributes__
 
-  @doc """
-  Hello world.
+  def get!(module, where) do
+    case get(module, where) do
+      nil -> raise_error(where, "not found")
+      value -> value
+    end
+  end
 
-  ## Examples
+  def get(module, where) do
+    module
+    |> attributes()
+    |> get_in(filter(where))
+  end
 
-      iex> Attributes.hello()
-      :world
+  def set!(module, where, value) do
+    case get(module, where) do
+      nil -> set(module, where, value)
+      _ -> raise_error(where, "already defined")
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def set(module, where, value) do
+    if not editable?(module) do
+      raise "#{module} already compiled."
+    end
+
+    if is_nil(Module.get_attribute(module, @attributes_field)) do
+      Module.register_attribute(module, @attributes_field, persist: true)
+    end
+
+    new_attributes =
+      module
+      |> attributes()
+      |> put_in(filter(where), value)
+
+    Module.put_attribute(module, @attributes_field, new_attributes)
+  end
+
+  defp attributes(module) do
+    if editable?(module) do
+      Module.get_attribute(module, @attributes_field, [])
+    else
+      :attributes
+      |> module.__info__()
+      |> Keyword.get(@attributes_field, [])
+    end
+  end
+
+  defp filter(where), do: Enum.flat_map(where, fn key -> [&handle_empty/3, key] end)
+
+  defp handle_empty(_, nil, next), do: next.([])
+  defp handle_empty(_, data, next), do: next.(data)
+
+  defp editable?(module), do: :elixir_module.mode(module) == :all
+
+  defp raise_error(keys, message) do
+    path = keys |> Enum.drop(-1) |> Enum.join(" -> ")
+    key = List.last(keys)
+
+    if path != "" do
+      raise "#{key} #{message} in #{path}."
+    else
+      raise "#{key} #{message}."
+    end
   end
 end
--- test/attributes_test.exs
+++ test/attributes_test.exs
@@ -1,8 +1,139 @@
 defmodule AttributesTest do
   use ExUnit.Case
-  doctest Attributes
 
-  test "greets the world" do
-    assert Attributes.hello() == :world
+  defmodule Dummy, do: :ok
+
+  describe "shallow" do
+    test "set different keys" do
+      defmodule ShallowSetDifferentKeys do
+        Attributes.set(__MODULE__, [:key], :value)
+        Attributes.set(__MODULE__, [:key2], :value2)
+      end
+
+      assert get_attrs(ShallowSetDifferentKeys) == [key2: :value2, key: :value]
+    end
+
+    test "set override" do
+      defmodule ShallowSetOverride do
+        Attributes.set(__MODULE__, [:key], :value)
+        Attributes.set(__MODULE__, [:key], :new_value)
+      end
+
+      assert get_attrs(ShallowSetOverride) == [key: :new_value]
+    end
+
+    test "set! different keys" do
+      defmodule ShallowSetBangDifferentKeys do
+        Attributes.set!(__MODULE__, [:key], :value)
+        Attributes.set!(__MODULE__, [:key2], :value2)
+      end
+
+      assert get_attrs(ShallowSetBangDifferentKeys) == [key2: :value2, key: :value]
+    end
+
+    test "set! raise on override" do
+      assert_raise RuntimeError, fn ->
+        defmodule ShallowSetRaise do
+          Attributes.set!(__MODULE__, [:key], :value)
+          Attributes.set!(__MODULE__, [:key], :new_value)
+        end
+      end
+    end
+
+    test "get" do
+      defmodule ShallowGet do
+        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+        @__attributes__ [key: :value]
+      end
+
+      assert Attributes.get(ShallowGet, [:key]) == :value
+    end
+
+    test "get nil" do
+      assert Attributes.get(Dummy, [:key]) == nil
+    end
+
+    test "get!" do
+      defmodule ShallowGetBang do
+        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+        @__attributes__ [key: :value]
+      end
+
+      assert Attributes.get!(ShallowGetBang, [:key]) == :value
+    end
+
+    test "get! raises on nil" do
+      assert_raise RuntimeError, fn ->
+        Attributes.get!(Dummy, [:key])
+      end
+    end
   end
+
+  describe "nested" do
+    test "set different keys" do
+      defmodule NestedSetDifferentKeys do
+        Attributes.set(__MODULE__, [:key, :subkey], :value)
+        Attributes.set(__MODULE__, [:key, :subkey2], :value2)
+      end
+
+      assert get_attrs(NestedSetDifferentKeys) == [key: [subkey2: :value2, subkey: :value]]
+    end
+
+    test "set override" do
+      defmodule NestedSetOverride do
+        Attributes.set(__MODULE__, [:key, :subkey], :value)
+        Attributes.set(__MODULE__, [:key, :subkey], :new_value)
+      end
+
+      assert get_attrs(NestedSetOverride) == [key: [subkey: :new_value]]
+    end
+
+    test "set! different keys" do
+      defmodule NestedSetBangDifferentKeys do
+        Attributes.set!(__MODULE__, [:key, :subkey], :value)
+        Attributes.set!(__MODULE__, [:key, :subkey2], :value2)
+      end
+
+      assert get_attrs(NestedSetBangDifferentKeys) == [key: [subkey2: :value2, subkey: :value]]
+    end
+
+    test "set! raise on override" do
+      assert_raise RuntimeError, fn ->
+        defmodule NestedSetRaise do
+          Attributes.set!(__MODULE__, [:key, :subkey], :value)
+          Attributes.set!(__MODULE__, [:key, :subkey], :new_value)
+        end
+      end
+    end
+
+    test "get" do
+      defmodule NestedGet do
+        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+        @__attributes__ [key: [subkey: :value]]
+      end
+
+      assert Attributes.get(NestedGet, [:key, :subkey]) == :value
+    end
+
+    test "get nil" do
+      assert Attributes.get(Dummy, [:key, :subkey]) == nil
+    end
+
+    test "get!" do
+      defmodule NestedGetBang do
+        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+        @__attributes__ [key: :value]
+      end
+
+      assert Attributes.get!(NestedGetBang, [:key]) == :value
+    end
+
+    test "get! raises on nil" do
+      assert_raise RuntimeError, fn ->
+        Attributes.get!(Dummy, [:key, :subkey])
+      end
+    end
+  end
+
+  defp get_attrs(module), do: module.__info__(:attributes)[:__attributes__]
 end
