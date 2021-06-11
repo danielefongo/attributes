6e48a8534218f83ec83cb157e31f74b0d88074cb
danielefongo <danielefongo@gmail.com>
2021-06-11 09:03:04 +0200

    reorganize tests

--- test/attributes_test.exs
+++ test/attributes_test.exs
@@ -3,358 +3,256 @@ defmodule AttributesTest do
 
   defmodule Dummy, do: :ok
 
-  describe "empty" do
-    test "set raise" do
+  describe "set" do
+    test "empty path raise" do
       assert_raise RuntimeError, fn ->
-        defmodule EmptySetRaise do
+        defmodule SetEmptyPathRaise do
           Attributes.set(__MODULE__, [], :value)
         end
       end
     end
 
-    test "set! raise" do
-      assert_raise RuntimeError, fn ->
-        defmodule EmptySetBangRaise do
-          Attributes.set!(__MODULE__, [], :value)
-        end
-      end
-    end
-
-    test "get raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.get(__MODULE__, [])
-      end
-    end
-
-    test "get! raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.get!(__MODULE__, [])
-      end
-    end
-
-    test "delete raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.delete(__MODULE__, [])
-      end
-    end
-
-    test "delete! raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.delete!(__MODULE__, [])
-      end
-    end
-
-    test "update raise" do
-      assert_raise RuntimeError, fn ->
-        defmodule EmptyUpdateRaise do
-          Attributes.update(__MODULE__, [], & &1)
-        end
-      end
-    end
-
-    test "update! raise" do
-      assert_raise RuntimeError, fn ->
-        defmodule EmptyUpdateBangRaise do
-          Attributes.update!(__MODULE__, [], & &1)
-        end
-      end
-    end
-  end
-
-  describe "shallow" do
-    test "set different keys" do
-      defmodule ShallowSetDifferentKeys do
+    test "different keys" do
+      defmodule SetDifferentKeys do
         Attributes.set(__MODULE__, [:key], :value)
         Attributes.set(__MODULE__, [:key2], :value2)
       end
 
-      assert get_attrs(ShallowSetDifferentKeys) == [key2: :value2, key: :value]
+      assert get_attrs(SetDifferentKeys) == [key2: :value2, key: :value]
     end
 
-    test "set override" do
-      defmodule ShallowSetOverride do
-        Attributes.set(__MODULE__, [:key], :value)
-        Attributes.set(__MODULE__, [:key], :new_value)
+    test "different nested keys" do
+      defmodule SetDifferentNestedKeys do
+        Attributes.set(__MODULE__, [:key, :subkey], :value)
+        Attributes.set(__MODULE__, [:key, :subkey2], :value2)
       end
 
-      assert get_attrs(ShallowSetOverride) == [key: :new_value]
+      assert get_attrs(SetDifferentNestedKeys) == [key: [subkey2: :value2, subkey: :value]]
     end
 
-    test "set! different keys" do
-      defmodule ShallowSetBangDifferentKeys do
-        Attributes.set!(__MODULE__, [:key], :value)
-        Attributes.set!(__MODULE__, [:key2], :value2)
+    test "override" do
+      defmodule SetOverride do
+        Attributes.set(__MODULE__, [:key, :subkey], :value)
+        Attributes.set(__MODULE__, [:key, :subkey], :new_value)
       end
 
-      assert get_attrs(ShallowSetBangDifferentKeys) == [key2: :value2, key: :value]
+      assert get_attrs(SetOverride) == [key: [subkey: :new_value]]
     end
 
-    test "set! raise on override" do
+    test "already compiled raise" do
       assert_raise RuntimeError, fn ->
-        defmodule ShallowSetRaise do
+        Attributes.set(Dummy, [:key], :value2)
+      end
+    end
+  end
+
+  describe "set!" do
+    test "raise when already defined" do
+      assert_raise RuntimeError, fn ->
+        defmodule SetBangDefinedRaise do
+          Attributes.set(__MODULE__, [:key], :value)
           Attributes.set!(__MODULE__, [:key], :value)
-          Attributes.set!(__MODULE__, [:key], :new_value)
         end
       end
     end
 
-    test "get" do
-      defmodule ShallowGet do
-        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ [key: :value]
+    test "when nil" do
+      defmodule SetBangNil do
+        Attributes.set!(__MODULE__, [:key], :value)
       end
 
-      assert Attributes.get(ShallowGet, [:key]) == :value
+      assert get_attrs(SetBangNil) == [key: :value]
+    end
+  end
+
+  describe "get" do
+    test "empty path raise" do
+      assert_raise RuntimeError, fn ->
+        Attributes.get(Dummy, [])
+      end
     end
 
-    test "get nil" do
-      assert Attributes.get(Dummy, [:key]) == nil
-    end
-
-    test "get default" do
-      assert Attributes.get(Dummy, [:key], :default) == :default
-    end
-
-    test "get!" do
-      defmodule ShallowGetBang do
+    test "before compile" do
+      defmodule GetBeforeCompile do
         Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ [key: :value]
+        @__attributes__ [key: [subkey: :value]]
+
+        @value Attributes.get(GetBeforeCompile, [:key, :subkey])
+
+        def gimme_value, do: @value
       end
 
-      assert Attributes.get!(ShallowGetBang, [:key]) == :value
+      assert GetBeforeCompile.gimme_value() == :value
     end
 
-    test "get! raises on nil" do
+    test "nested" do
+      defmodule GetNested do
+        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+        @__attributes__ [key: [subkey: :value]]
+      end
+
+      assert Attributes.get(GetNested, [:key, :subkey]) == :value
+    end
+
+    test "nil" do
+      assert Attributes.get(Dummy, [:key, :subkey]) == nil
+    end
+
+    test "default" do
+      assert Attributes.get(Dummy, [:key, :subkey], :default) == :default
+    end
+  end
+
+  describe "get!" do
+    test "raise when not found" do
       assert_raise RuntimeError, fn ->
         Attributes.get!(Dummy, [:key])
       end
     end
 
-    test "delete" do
-      defmodule ShallowDelete do
-        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ [key: :value, key2: :value2]
-        Attributes.delete(ShallowDelete, [:key])
-      end
-
-      assert get_attrs(ShallowDelete) == [key2: :value2]
-    end
-
-    test "delete not existing" do
-      defmodule ShallowDeleteNotExisting do
-        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ []
-
-        Attributes.delete(ShallowDeleteNotExisting, [:key])
-      end
-
-      assert get_attrs(ShallowDeleteNotExisting) == []
-    end
-
-    test "delete!" do
-      defmodule ShallowDeleteBang do
-        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ [key: :value, key2: :value2]
-        Attributes.delete!(ShallowDeleteBang, [:key])
-      end
-
-      assert get_attrs(ShallowDeleteBang) == [key2: :value2]
-    end
-
-    test "delete! raise on nil" do
+    test "raise when nil" do
       assert_raise RuntimeError, fn ->
-        defmodule ShallowDeleteBangRaise do
+        defmodule GetBangNil do
           Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-          @__attributes__ []
-          Attributes.delete!(ShallowDeleteBangRaise, [:key])
+          @__attributes__ [key: nil]
+          Attributes.get!(Dummy, [:key])
         end
       end
     end
 
-    test "update" do
-      defmodule ShallowUpdate do
-        Attributes.set(__MODULE__, [:key], 41)
-        Attributes.update(__MODULE__, [:key], &(&1 + 1))
-      end
-
-      assert get_attrs(ShallowUpdate) == [key: 42]
-    end
-
-    test "update nil" do
-      defmodule ShallowUpdateNil do
-        Attributes.update(__MODULE__, [:key], fn _ -> 42 end)
-      end
-
-      assert get_attrs(ShallowUpdateNil) == [key: 42]
-    end
-
-    test "update!" do
-      defmodule ShallowUpdateBang do
-        Attributes.set(__MODULE__, [:key], 41)
-        Attributes.update!(__MODULE__, [:key], &(&1 + 1))
-      end
-
-      assert get_attrs(ShallowUpdateBang) == [key: 42]
-    end
-
-    test "set! raise on nil" do
-      assert_raise RuntimeError, fn ->
-        defmodule ShallowUpdateBangRaise do
-          Attributes.set(__MODULE__, [:key], nil)
-          Attributes.update!(__MODULE__, [:key], &(&1 + 1))
-        end
-      end
-    end
-  end
-
-  describe "nested" do
-    test "set different keys" do
-      defmodule NestedSetDifferentKeys do
-        Attributes.set(__MODULE__, [:key, :subkey], :value)
-        Attributes.set(__MODULE__, [:key, :subkey2], :value2)
-      end
-
-      assert get_attrs(NestedSetDifferentKeys) == [key: [subkey2: :value2, subkey: :value]]
-    end
-
-    test "set override" do
-      defmodule NestedSetOverride do
-        Attributes.set(__MODULE__, [:key, :subkey], :value)
-        Attributes.set(__MODULE__, [:key, :subkey], :new_value)
-      end
-
-      assert get_attrs(NestedSetOverride) == [key: [subkey: :new_value]]
-    end
-
-    test "set! different keys" do
-      defmodule NestedSetBangDifferentKeys do
-        Attributes.set!(__MODULE__, [:key, :subkey], :value)
-        Attributes.set!(__MODULE__, [:key, :subkey2], :value2)
-      end
-
-      assert get_attrs(NestedSetBangDifferentKeys) == [key: [subkey2: :value2, subkey: :value]]
-    end
-
-    test "set! raise on override" do
-      assert_raise RuntimeError, fn ->
-        defmodule NestedSetRaise do
-          Attributes.set!(__MODULE__, [:key, :subkey], :value)
-          Attributes.set!(__MODULE__, [:key, :subkey], :new_value)
-        end
-      end
-    end
-
-    test "get" do
-      defmodule NestedGet do
-        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ [key: [subkey: :value]]
-      end
-
-      assert Attributes.get(NestedGet, [:key, :subkey]) == :value
-    end
-
-    test "get nil" do
-      assert Attributes.get(Dummy, [:key, :subkey]) == nil
-    end
-
-    test "get default" do
-      assert Attributes.get(Dummy, [:key, :subkey], :default) == :default
-    end
-
-    test "get!" do
-      defmodule NestedGetBang do
+    test "when defined" do
+      defmodule GetBangDefined do
         Module.register_attribute(__MODULE__, :__attributes__, persist: true)
         @__attributes__ [key: :value]
       end
 
-      assert Attributes.get!(NestedGetBang, [:key]) == :value
+      assert Attributes.get!(GetBangDefined, [:key]) == :value
     end
+  end
 
-    test "get! raises on nil" do
+  describe "delete" do
+    test "empty path raise" do
       assert_raise RuntimeError, fn ->
-        Attributes.get!(Dummy, [:key, :subkey])
+        Attributes.delete(Dummy, [])
       end
     end
 
-    test "delete" do
-      defmodule NestedDelete do
+    test "nested" do
+      defmodule DeleteNested do
         Module.register_attribute(__MODULE__, :__attributes__, persist: true)
         @__attributes__ [key: [subkey: :value, subkey2: :value2]]
-        Attributes.delete(NestedDelete, [:key, :subkey])
+        Attributes.delete(DeleteNested, [:key, :subkey2])
       end
 
-      assert get_attrs(NestedDelete) == [key: [subkey2: :value2]]
+      assert get_attrs(DeleteNested) == [key: [subkey: :value]]
     end
 
-    test "delete not existing" do
-      defmodule NestedDeleteNotExisting do
-        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ []
-
-        Attributes.delete(NestedDeleteNotExisting, [:key, :subkey])
+    test "not existing" do
+      defmodule DeleteNotExisting do
+        Attributes.delete(DeleteNotExisting, [:key])
       end
 
-      assert get_attrs(NestedDeleteNotExisting) == []
+      assert get_attrs(DeleteNotExisting) == []
     end
 
-    test "delete!" do
-      defmodule NestedDeleteBang do
-        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-        @__attributes__ [key: [subkey: :value, subkey2: :value2]]
-        Attributes.delete!(NestedDeleteBang, [:key, :subkey])
-      end
-
-      assert get_attrs(NestedDeleteBang) == [key: [subkey2: :value2]]
-    end
-
-    test "delete! raise on nil" do
+    test "already compiled raise" do
       assert_raise RuntimeError, fn ->
-        defmodule NestedDeleteBangRaise do
-          Module.register_attribute(__MODULE__, :__attributes__, persist: true)
-          @__attributes__ []
-          Attributes.delete!(NestedDeleteBangRaise, [:key, :subkey])
-        end
-      end
-    end
-
-    test "update" do
-      defmodule NestedUpdate do
-        Attributes.set(__MODULE__, [:key, :subkey], 41)
-        Attributes.update(__MODULE__, [:key, :subkey], &(&1 + 1))
-      end
-
-      assert get_attrs(NestedUpdate) == [key: [subkey: 42]]
-    end
-
-    test "update nil" do
-      defmodule NestedUpdateNil do
-        Attributes.update(__MODULE__, [:key, :subkey], fn _ -> 42 end)
-      end
-
-      assert get_attrs(NestedUpdateNil) == [key: [subkey: 42]]
-    end
-
-    test "update!" do
-      defmodule NestedUpdateBang do
-        Attributes.set(__MODULE__, [:key, :subkey], 41)
-        Attributes.update!(__MODULE__, [:key, :subkey], &(&1 + 1))
-      end
-
-      assert get_attrs(NestedUpdateBang) == [key: [subkey: 42]]
-    end
-
-    test "set! raise on nil" do
-      assert_raise RuntimeError, fn ->
-        defmodule NestedUpdateBangRaise do
-          Attributes.set(__MODULE__, [:key, :subkey], nil)
-          Attributes.update!(__MODULE__, [:key, :subkey], &(&1 + 1))
-        end
+        Attributes.delete(Dummy, [:key, :subkey])
       end
     end
   end
 
+  describe "delete!" do
+    test "raise when not found" do
+      assert_raise RuntimeError, fn ->
+        Attributes.delete!(Dummy, [:key])
+      end
+    end
+
+    test "raise when nil" do
+      assert_raise RuntimeError, fn ->
+        defmodule DeleteBangNil do
+          Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+          @__attributes__ [key: nil]
+          Attributes.get!(Dummy, [:key])
+        end
+      end
+    end
+
+    test "when defined" do
+      defmodule DeleteBangDefined do
+        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+        @__attributes__ [key: :value]
+        Attributes.delete!(DeleteBangDefined, [:key])
+      end
+
+      assert get_attrs(DeleteBangDefined) == []
+    end
+  end
+
+  describe "update" do
+    test "empty path raise" do
+      assert_raise RuntimeError, fn ->
+        defmodule UpdateEmptyPathRaise do
+          Attributes.update(__MODULE__, [], & &1)
+        end
+      end
+    end
+
+    test "nested" do
+      defmodule UpdateNested do
+        Attributes.set(__MODULE__, [:key, :subkey], 41)
+        Attributes.update(__MODULE__, [:key, :subkey], &(&1 + 1))
+      end
+
+      assert get_attrs(UpdateNested) == [key: [subkey: 42]]
+    end
+
+    test "nil" do
+      defmodule UpdateNil do
+        Attributes.update(__MODULE__, [:key, :subkey], fn _ -> 42 end)
+      end
+
+      assert get_attrs(UpdateNil) == [key: [subkey: 42]]
+    end
+
+    test "already compiled raise" do
+      assert_raise RuntimeError, fn ->
+        Attributes.update(Dummy, [:key], fn _ -> :any end)
+      end
+    end
+  end
+
+  describe "update!" do
+    test "raise when not found" do
+      assert_raise RuntimeError, fn ->
+        Attributes.update!(Dummy, [:key], & &1)
+      end
+    end
+
+    test "raise when nil" do
+      assert_raise RuntimeError, fn ->
+        defmodule UpdateBangNil do
+          Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+          @__attributes__ [key: nil]
+          Attributes.update!(__MODULE__, [:key], & &1)
+        end
+      end
+    end
+
+    test "when defined" do
+      defmodule UpdateBangDefined do
+        Module.register_attribute(__MODULE__, :__attributes__, persist: true)
+        @__attributes__ [key: :value]
+        Attributes.update!(__MODULE__, [:key], fn _ -> :new_value end)
+      end
+
+      assert get_attrs(UpdateBangDefined) == [key: :new_value]
+    end
+  end
+
   describe "hybrid" do
-    test "set override inside keyword" do
+    test "override inside keyword" do
       defmodule HybridSetOverrideInsideKeyword do
         Attributes.set(__MODULE__, [:key], [])
         Attributes.set(__MODULE__, [:key, :subkey], :value)
@@ -363,7 +261,7 @@ defmodule AttributesTest do
       assert get_attrs(HybridSetOverrideInsideKeyword) == [key: [subkey: :value]]
     end
 
-    test "set override inside map" do
+    test "override inside map" do
       defmodule HybridSetOverrideInsideMap do
         Attributes.set(__MODULE__, [:key], %{})
         Attributes.set(__MODULE__, [:key, :subkey], :value)
@@ -397,32 +295,6 @@ defmodule AttributesTest do
       end
 
       assert get_attrs(HybridSetOverrideOuterMap) == [key: %{data: :new}]
-    end
-  end
-
-  describe "already compiled" do
-    test "set raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.set(Dummy, [:key], :value2)
-      end
-    end
-
-    test "set! raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.set!(Dummy, [:key], :value2)
-      end
-    end
-
-    test "update raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.update(Dummy, [:key], fn _ -> :any end)
-      end
-    end
-
-    test "update! raise" do
-      assert_raise RuntimeError, fn ->
-        Attributes.update!(Dummy, [:key], fn _ -> :any end)
-      end
     end
   end
 
